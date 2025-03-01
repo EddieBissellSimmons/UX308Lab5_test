7b93a26a91b3053209c49f19ee2e8f5e6e86f1d3
EddieBissellSimmons <144965190+EddieBissellSimmons@users.noreply.github.com>
2025-03-01 03:07:06 +0000

    add q 1, 2, 3

--- functions.js
+++ functions.js
@@ -1,6 +1,40 @@
-function hello(){
-    return "hello";
+//question 1
+function convertFeetToAcres(feet){
+    let acres = feet/43560;
+    return acres;
 }
 
+//question 2
+function cutlawn(width, length, cutrate){
+    let minutes = (width*length)/cutrate;
+    return minutes;
+}
 
-export {hello}
+//question 3
+function airquality(quality){
+    let description;
+    if (quality <= 50){
+        description = "good";
+    } else if (quality >= 51 && quality <= 100){
+        description = "moderate";
+    } else if (quality >= 101 && quality <= 150){
+        description = "unhealthy for sensitive groups"
+    } else if (quality >= 151 && quality <= 200){
+        description = "unhealthy"
+    } else if (quality >= 201 && quality <= 300){
+        description = "very unhealthy"
+    } else if (quality > 300){
+        description = "hazardous"
+    }
+    return description;
+}
+
+//question 4
+
+//question 5
+
+
+//exporting all of the functions
+export {convertFeetToAcres}
+export {cutlawn}
+export {airquality}
--- spec/functions_spec.js
+++ spec/functions_spec.js
@@ -1,10 +1,86 @@
-import { hello } from '../functions.js'
+import { convertFeetToAcres } from "../functions.js";
+import { cutlawn } from "../functions.js";
+import { airquality } from "../functions.js";
 
-describe("A suite of tests for functions.js", function() {
-    it('tests for a function named hello that returns "hello"', function() {
-      let sHello = hello();
-      expect(sHello).toEqual("hello");
+//question 1
+describe("text feet to acres", function() {
+    it(" tesing 43560 feet  ", function () {
+        let feet = 43560;
+        let acres = convertFeetToAcres(feet);
+        expect(acres).toBe(1);
     });
+    it(" testing 87120 feet ", function () {
+      let feet = 87120;
+      let acres = convertFeetToAcres(feet);
+      expect(acres).toBe(2);
   });
-  
-  
+  it(" testing 130680 feet ", function () {
+    let feet = 130680;
+    let acres = convertFeetToAcres(feet);
+    expect(acres).toBe(3);
+});
+});
+
+//question 2
+describe("test cutting the lawn", function() {
+  it(" a 2x2 lawn, cut at 2/min ", function () {
+      let width = 2;
+      let length = 2;
+      let cutrate = 2;
+      let minutes = cutlawn(width,length,cutrate);
+      expect(minutes).toBe(2);
+  });
+  it(" a 16x16 lawn, cut at 3/min ", function () {
+    let width = 16;
+    let length = 16;
+    let cutrate = 4;
+    let minutes = cutlawn(width,length,cutrate);
+    expect(minutes).toBe(64);
+});
+it(" a 10x20 lawn, cut at 0.5/min ", function () {
+  let width = 10;
+  let length = 20;
+  let cutrate = 0.5;
+  let minutes = cutlawn(width,length,cutrate);
+  expect(minutes).toBe(400);
+});
+});
+
+//question 3
+describe("test the air quality", function() {
+  it(" if the air quality is 5 ", function () {
+    let quality = 5;
+    let description = airquality(quality);
+    expect(description).toBe("good");
+  });
+  it(" if the air quality is 75 ", function () {
+      let quality = 75;
+      let description = airquality(quality);
+      expect(description).toBe("moderate");
+  });
+  it(" if the air quality is 143 ", function () {
+    let quality = 143;
+    let description = airquality(quality);
+    expect(description).toBe("unhealthy for sensitive groups");
+  });
+  it(" if the air quality is 173 ", function () {
+    let quality = 173;
+    let description = airquality(quality);
+    expect(description).toBe("unhealthy");
+  });
+  it(" if the air quality is 233 ", function () {
+    let quality = 233;
+    let description = airquality(quality);
+    expect(description).toBe("very unhealthy");
+  });
+  it(" if the air quality is 302 ", function () {
+    let quality = 302;
+    let description = airquality(quality);
+    expect(description).toBe("hazardous");
+  });
+});
+
+//question 4
+
+
+//question 5
